914dfc943c0d9c02b5e611bde5a5bdde67306598
vivekkumar333 <tiwari.vivekkumar333@gmail.com>
2024-06-09 11:33:32 +0530

    Rename application name job-rating-service-registry to service-registry

--- service-registry/src/main/java/com/learnwithtiwari/service/registory/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/learnwithtiwari/service/registory/ServiceRegistryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class JobCompanyRatingServiceRegistryApplication {
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobCompanyRatingServiceRegistryApplication.class, args);
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
